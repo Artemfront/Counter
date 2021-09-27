4fa3e3d2743f02e191c415e8b71a56a656738286
Artem <arpavlovscky@yandex.ru>
2021-09-27 19:50:25 +0300

    finished

---
+++ app.js
@@ -0,0 +1,32 @@
+(function() {
+    let buttons = document.querySelectorAll('.btn')
+    let count = 0
+
+    // adding events for all buttons and create functionality
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if(button.classList.contains('LowerCount')) {
+                count--
+            } else {
+                count++
+            }
+
+
+            // select the counter text
+            const counter = document.querySelector('#counter')
+            counter.textContent = count
+            if(count > 0) {
+                counter.style.color = "green"
+            } else if(count < 0) {
+                counter.style.color = "red"
+            } else {
+                counter.style.color = 'black'
+            }
+        })
+    })
+
+
+    
+
+})()
+
